7cb0b0d0e2999d55caddb21615964fc04bedd5b5
Kush2806 <ladolakush66@gmail.com>
2023-04-30 05:07:56 +0530

    pdf-complition-almost

--- src/Trial.jsx
+++ src/Trial.jsx
@@ -3,16 +3,13 @@ import { Document,Image, Page, Text, PDFDownloadLink } from '@react-pdf/renderer
 import { View } from '@react-pdf/renderer';
 
 function Trial() {
-  // Define the static data
   const data = {
     title: 'Invoice',
     content: 'This is some static data that will be included in the PDF document.'
   };
 
-  // Define a state variable to keep track of whether the PDF document should be generated
   const [generatePDF, setGeneratePDF] = useState(false);
 
-  // Define a function to handle the button click event and set the generatePDF state variable to true
   const handleClick = () => {
     setGeneratePDF(true);
   };
@@ -40,9 +37,41 @@ function Trial() {
     <Document>
       <Page size="A4">
         <View style={{margin:10,border:2,height:830,width:575}}>
-          <View>
+          <View style={{border:1,width:573,marginTop:100,height:0}}>
             <img src="C:\Users\Kush\Documents\Python\twitter1.png"/>
+            <View>
+            <Text style={{margin:10}}>Buyer's Name:</Text>
+            <Text style={{margin:10,marginTop:20}}>Address:</Text>
+            <Text style={{margin:10,marginTop:20}}>GST No :</Text>
+            </View>
+            <View style={{marginLeft:400}}>
+              <Text style={{marginTop:-85}}>Name:</Text>
+              <Text>Address:</Text>
+              <Text>Ph-no:</Text>
+            </View>
+            <View style={{border:1,marginTop:600}}>
+            </View>
+            <View style={{marginTop:-600,marginLeft:400}}>
+              <Text style={{marginTop:10}}>Invoice No:</Text>
+              <Text style={{marginTop:30}}>Invoice Date:</Text>
+            </View>
           </View>
+          <View style={{border:1,width:573,marginTop:100,height:0}}>
+            <Text style={{marginLeft:10}}>Sr.No: </Text>
+          </View>
+          <View style={{marginLeft:100}}>
+          <Text style={{marginLeft:10}}>Image</Text>
+          </View>
+          <View style={{marginLeft:220,marginTop:-18}}> 
+          <Text>Qt</Text>
+          </View>
+          <View style={{marginLeft:330, marginTop:-20}}>
+          <Text>Price</Text>
+          </View>
+          <View style={{marginLeft:450,marginTop:-21}}>
+          <Text>Amount</Text>
+          </View>
+          <View style={{marginTop:400}}></View>
         </View>
       </Page>
     </Document>
