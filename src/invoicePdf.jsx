ac9246fda77ae893f430b95788967586a1d5caa8
Mohit <mohit.patel1966@gmail.com>
2023-04-30 13:32:52 +0530

    dummy pdf added

--- src/invoicePdf.jsx
+++ src/invoicePdf.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Document,Image, Page, Text, PDFDownloadLink } from '@react-pdf/renderer';
 import { View } from '@react-pdf/renderer';
 
-function InvoicePdf(...props) {
+function InvoicePdf() {
   const data = {
     title: 'Invoice',
     content: 'This is some static data that will be included in the PDF document.'
@@ -40,39 +40,50 @@ function InvoicePdf(...props) {
           <View style={{border:1,width:573,marginTop:100,height:0}}>
             <img src="C:\Users\Kush\Documents\Python\twitter1.png"/>
             <View>
-            <Text style={{margin:10}}>Buyer's Name:</Text>
-            <Text style={{margin:10,marginTop:20}}>Address:</Text>
-            <Text style={{margin:10,marginTop:20}}>GST No :</Text>
+            <Text style={{margin:10}}>Buyer's Name: Namit Patel</Text>
+            <Text style={{margin:10,marginTop:20}}>Address: Surat</Text>
+            <Text style={{margin:10,marginTop:20}}>GST No :O7ATNPB9726C1ZP</Text>
             </View>
-            <View style={{marginLeft:400}}>
-              <Text style={{marginTop:-85,}}>Name:</Text>
-              <Text>Address:</Text>
-              <Text>Ph-no:</Text>
+            <View style={{marginLeft:385}}>
+              <Text style={{marginTop:-85,}}>Name: Kush</Text>
+              <Text>Address: Ahmedabad</Text>
+              <Text>Ph-no: 6355653355</Text>
             </View>
-            <View style={{marginLeft:410}}><Text style={{marginTop:605}}>Total Amt:</Text></View>
+            <View style={{marginLeft:410}}><Text style={{marginTop:605}}>Total Amt:48,000</Text></View>
             <View style={{border:1,marginTop:600}}>
             </View>
-            <View style={{marginTop:-600,marginLeft:400}}>
-              <Text style={{marginTop:10,}}>Invoice No:</Text>
-              <Text style={{marginTop:30,}}>Invoice Date:</Text>
+            <View style={{marginTop:-600,marginLeft:360}}>
+              <Text style={{marginTop:10,}}>Invoice No: SI789</Text>
+              <Text style={{marginTop:30,}}>Invoice Date:21-04-2023</Text>
             </View>
           </View>
           <View style={{border:1,width:573,marginTop:100,height:0}}>
-            <Text style={{marginLeft:10,}}>Sr.No: </Text>
+            <Text style={{marginLeft:10}}>Sr.No:</Text>
+            <Text style={{marginTop:30,marginLeft:30}}>1</Text>
+            <Text style={{marginTop:100,marginLeft:30}}>2</Text>
           </View>
           <View style={{marginLeft:100}}>
           <Text style={{marginLeft:10}}>Image</Text>
           </View>
           <View style={{marginLeft:220,marginTop:-18}}> 
           <Text>Qt</Text>
+          <Text style={{marginTop:10}}>200</Text>
+          <Text style={{marginTop:80}}>400</Text>
           </View>
-          <View style={{marginLeft:330, marginTop:-20}}>
+          <View style={{marginLeft:330, marginTop:-150}}>
           <Text>Price</Text>
+          <Text style={{marginTop:10}}>1200</Text>
+          <Text style={{marginTop:80}}>600</Text>
           </View>
-          <View style={{marginLeft:450,marginTop:-21}}>
+          <View style={{marginLeft:450,marginTop:-150}}>
           <Text>Amount</Text>
+          <Text style={{marginTop:10}}>24,000</Text>
+          <Text style={{marginTop:80}}>24,000</Text>
           </View>
           <View style={{marginTop:400}}></View>
+        <View style={{marginTop:-720,marginLeft:180}}>
+          <Text>Shri Ram Informatics</Text>
+        </View>
         </View>
       </Page>
     </Document>
